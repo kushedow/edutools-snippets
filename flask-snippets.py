0c9f64c364008e5894165b992a06bb1c86023bb2
Gleb Kushedow <kushedow@gmail.com>
2021-09-06 17:23:11 +0300

    Update flask-snippets.py
    Initial

--- flask-snippets.py
+++ flask-snippets.py
@@ -1,1 +1,37 @@
+# simple flask app
 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def sample():
+    result = []
+    return dumps(result), 200
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+# check
+
+from task import app
+
+class TestCase(TestCase):
+    def test_sort(self):
+        with app.test_client() as client:
+
+            #  проверка статуса
+
+            resp = client.get('/sort/')
+            self.assertNotEqual(404, resp.status_code, msg='Представление должно работать по урлу /sort/')
+
+            # ожидание и реальность
+
+            path = '/'
+            expected = '...'
+            resp = client.get(path)
+            self.assertEqual(expected, resp.get_data(True),
+                             msg=f'Неверный ответ пи запросе к {path} должно быть {expected}')
